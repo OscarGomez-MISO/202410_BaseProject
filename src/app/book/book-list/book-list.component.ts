ad317c24a0ab9d03eb693e052c3351d91de863b1
Oscar Jobany Gomez Ochoa <ogomezo@minsalud.gov.co>
2025-11-04 21:25:16 -0500

    Tutoriales

---
+++ src/app/book/book-list/book-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../book';
+import { BookService } from '../book.service';
+import { BookDetail } from '../BookDetail';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent implements OnInit {
+
+  books: Array<BookDetail> = [];
+  selected: Boolean = false;
+  selectedBook!: BookDetail; 
+
+  constructor(private bookService: BookService) { }
+
+  getBooks(): void {
+    this.bookService.getBooks().subscribe((books) => {
+      this.books = books;
+    });
+  }
+
+  onSelected(book: BookDetail): void {
+    this.selected = true;
+    this.selectedBook = book;
+  }
+
+  ngOnInit() {
+    this.getBooks();
+  }
+}
---
+++ src/app/book/book.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment.development';
+import { Observable } from 'rxjs';
+import { Book } from './book';
+import { BookDetail } from './BookDetail';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+
+  private apiUrl: string = environment.baseUrl + 'books';
+
+  constructor(private http: HttpClient) { }
+
+  getBooks(): Observable<BookDetail[]> {
+    return this.http.get<BookDetail[]>(this.apiUrl);
+  }
+}
